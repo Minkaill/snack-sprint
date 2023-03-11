b2a5a4bd3206ecd1185370a7d7ee0fdeffa7a38a
Wiynxr <mkx.dake@gmail.com>
2023-03-11 16:33:25 +0300

    Добавил и настроил hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import { useForm, UseFormRegister } from "react-hook-form";
 import styles from "../styles/Form.module.scss";
 import Input from "./Input";
-
-interface IUser {
-  name: string;
-  phone: string;
-  city: string;
-  adress: string;
-  email: string;
-  password: string;
-  restaurant: string;
-}
+import { IUser } from "../types/IUser";
+import { FormValues } from "../types/FormValues";
 
 interface IForm {
   select: string;
 }
 
 const Form: React.FC<IForm> = ({ select }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormValues>({
+    mode: "onBlur",
+  });
+  const onSubmit = () => console.log();
+
   const [user, setUser] = React.useState<IUser>({
     name: "",
     phone: "",
@@ -38,51 +40,89 @@ const Form: React.FC<IForm> = ({ select }) => {
   };
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
       {select === "Стать клиентом" ? (
-        <Input
-          name="name"
-          label="Имя"
-          value={user.name}
-          handleUserChange={handleUserChange}
-        />
+        <>
+          <Input
+            register={register}
+            name="name"
+            label="Имя"
+            value={user.name}
+            handleUserChange={handleUserChange}
+          />
+          <div className={styles.error}>
+            {errors?.name && <p>{errors?.name.message}</p>}
+          </div>
+        </>
       ) : (
-        <Input
-          name="restaurant"
-          label="Название ресторана"
-          value={user.restaurant}
-          handleUserChange={handleUserChange}
-        />
+        <>
+          <Input
+            register={register}
+            name="restaurant"
+            label="Название ресторана"
+            value={user.restaurant}
+            handleUserChange={handleUserChange}
+          />
+          <div className={styles.error}>
+            {errors?.restaurant && <p>{errors?.restaurant.message}</p>}
+          </div>
+        </>
       )}
       <Input
+        register={register}
         name="phone"
         label="Номер телефона"
         value={user.phone}
         handleUserChange={handleUserChange}
       />
+      <div className={styles.error}>
+        {errors?.phone && <p>{errors?.phone.message}</p>}
+      </div>
       <Input
+        register={register}
         name="city"
         label="Город"
         value={user.city}
         handleUserChange={handleUserChange}
       />
+      <div className={styles.error}>
+        {errors?.city && <p>{errors?.city.message}</p>}
+      </div>
       <Input
+        register={register}
         name="adress"
         label="Адресс"
         value={user.adress}
         handleUserChange={handleUserChange}
       />
+      <div className={styles.error}>
+        {errors?.adress && <p>{errors?.adress.message}</p>}
+      </div>
       <Input
+        register={register}
         name="email"
         label="Электронная почта"
         value={user.email}
         handleUserChange={handleUserChange}
       />
+      <div className={styles.error}>
+        {errors?.email && <p>{errors?.email.message}</p>}
+      </div>
       <Input
+        register={register}
         name="password"
         label="Пароль"
         value={user.password}
         handleUserChange={handleUserChange}
+      />
+      <div className={styles.error}>
+        {errors?.password && <p>{errors?.password.message}</p>}
+      </div>
+      <input
+        disabled={!isValid}
+        type="submit"
+        value="Регистрация"
+        name="send"
       />
     </form>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import styles from "../styles/Input.module.scss";
+import { UseFormRegister } from "react-hook-form";
+import { IUser } from "../types/IUser";
+import { FormValues } from "../types/FormValues";
 
 interface IInput {
-  name: string;
+  name: any;
   label: string;
   value: string;
   handleUserChange: React.ChangeEventHandler<HTMLInputElement>;
+  register: UseFormRegister<FormValues>;
 }
 
-const Input: React.FC<IInput> = ({ value, handleUserChange, label, name }) => {
+const Input: React.FC<IInput> = ({
+  value,
+  handleUserChange,
+  label,
+  name,
+  register,
+}) => {
   return (
     <div className={styles.container}>
       <label>{label}</label>
       <input
+        {...register(name, {
+          required: "Поле обязательно к заполнению.",
+          minLength: {
+            value: 5,
+            message: "Минимум 5 символов.",
+          },
+        })}
+        id={name}
         name={name}
         onChange={handleUserChange}
         type="text"
